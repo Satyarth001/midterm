52628edb4285c79c7e151d67b89904294fcae311
Satyarth001 <satyarth001@algomau.ca>
2025-02-25 13:47:24 -0500

    Create darkmodetoggle.js

---
+++ js/darkmodetoggle.js
@@ -0,0 +1,33 @@
+
+function calculateFactorial(n) {
+    let factorial = 1;
+    for (let i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+
+document.getElementById('enterButton').addEventListener('click', function() {
+    const numberInput = document.getElementById('numberInput').value;
+    const resultScreen = document.getElementById('resultScreen');
+    
+    if (numberInput) {
+        const factorial = calculateFactorial(parseInt(numberInput));
+        resultScreen.textContent = `${numberInput}! = ${factorial}`;
+    } else {
+        resultScreen.textContent = 'Please enter a number.';
+    }
+});
+
+
+const darkButton = document.querySelector('[data-bs-theme-value="dark"]');
+const lightButton = document.querySelector('[data-bs-theme-value="light"]');
+
+darkButton.addEventListener('click', function() {
+    document.body.setAttribute('data-bs-theme', 'dark');
+});
+
+lightButton.addEventListener('click', function() {
+    document.body.setAttribute('data-bs-theme', 'light');
+});
